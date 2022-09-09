af1355ac8bc7bad2133829b07be476e91f066735
Brian <brian.lee@premise.com>
2022-09-09 19:29:30 -0400

    Update request.py

--- slack_approval/functions/request.py
+++ slack_approval/functions/request.py
@@ -8,11 +8,11 @@ goblet_entrypoint(app)
 
 @app.http()
 def main(request):
-    """Forwards requests to slack
+    """Forwards requests to slack.
     """
     slack_request = SlackRequest(
         request,
         os.environ.get("APPROVERS_CHANNEL_HOOK"),
-        os.environ.get("REQUESTER_CHANNEL_HOOK"),
+        os.environ.get("REQUESTERS_CHANNEL_HOOK"),
     )
     slack_request.send_request_message()
