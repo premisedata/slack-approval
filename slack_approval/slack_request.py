32eee44d767a34bf89afcd11e571af7bf81ff008
dmutia <dennis.mutia.c@premise.com>
2023-03-02 23:04:56 +0300

    hide approving team

--- slack_approval/slack_request.py
+++ slack_approval/slack_request.py
@@ -13,7 +13,7 @@ class SlackRequest:
         """
         self.inputs = request.json
         self.name = self.inputs["provision_class"]
-        # self.approving_team = self.inputs.get("approving_team", None)
+        self.approving_team = self.inputs.get("approving_team", None)
         self.value = self.inputs # save inputs before hiding anything
         hide = self.inputs.get("hide")
         if hide:
@@ -21,8 +21,9 @@ class SlackRequest:
             for field in hide:
                 self.inputs.pop(field)
             self.inputs.pop("hide")
+        self.inputs.pop("approving_team")
         self.token = os.environ.get("SLACK_BOT_TOKEN")
-        self.approvers_channel, self.requesters_channel = self.get_slack_channels()
+        self.approvers_channel, self.requesters_channel = self.get_slack_channels(self.approving_team)
 
     def send_request_message(self):
         slack_web_client = WebClient(self.token)
@@ -111,10 +112,8 @@ class SlackRequest:
         except errors.SlackApiError as e:
             logger.error(e)
 
-    def get_slack_channels(self):
+    def get_slack_channels(self, approving_team):
         """Get approvers and requesters channels from environment variables."""
-        
-        approving_team = self.inputs.get("approving_team", None)
         try:
             if approving_team is None:
                 approvers_channel = os.environ["APPROVERS_CHANNEL"]
