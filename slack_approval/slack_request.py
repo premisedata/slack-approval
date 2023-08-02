1bcd633bb2e96ad91bacacb0dc612aa2999b5002
Diego Diaz <diegodiazespinoza@gmail.com>
2023-08-02 19:18:25 -0400

    [TEST]

--- slack_approval/slack_request.py
+++ slack_approval/slack_request.py
@@ -110,21 +110,21 @@ class SlackRequest:
                             "confirm": {"type": "plain_text", "text": "Do it"},
                             "deny": {
                                 "type": "plain_text",
-                                "text": "Stop, I've changed my mind!",
-                                "reason": {
-                                    {
-                                        "type": "input",
-                                        "label": {
+                                "text": "Stop, I've changed my mind!"
+                            },
+                            "reason": {
+                                {
+                                    "type": "input",
+                                    "label": {
+                                        "type": "plain_text",
+                                        "text": "Reject"
+                                    },
+                                    "element": {
+                                        "type": "plain_text_input",
+                                        "action_id": "reason_response",
+                                        "placeholder": {
                                             "type": "plain_text",
-                                            "text": "Reject"
-                                        },
-                                        "element": {
-                                            "type": "plain_text_input",
-                                            "action_id": "reason_response",
-                                            "placeholder": {
-                                                "type": "plain_text",
-                                                "text": "Type reason"
-                                            }
+                                            "text": "Type reason"
                                         }
                                     }
                                 }
