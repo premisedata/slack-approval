c437625bfc62471c1756bad451edc044cc61634c
Diego Diaz <diegodiazespinoza@gmail.com>
2023-08-04 17:15:56 -0400

    [TEST]

--- slack_approval/functions/provision.py
+++ slack_approval/functions/provision.py
@@ -4,25 +4,16 @@ from slack_approval.slack_provision import SlackProvision
 
 app = Goblet(function_name="provision")
 goblet_entrypoint(app)
-import logging
 
-logger = logging.getLogger("slack_provision")
-logger.setLevel(logging.DEBUG)
+
 @app.http()
 def main(request):
     """
     """
     slack_provision = SlackProvision(request)
     # validate request using the signature secret
-    # if not slack_provision.is_valid_signature(os.environ.get("SIGNING_SECRET")):
-    #     return Response("Forbidden", status_code=403)
+    if not slack_provision.is_valid_signature(os.environ.get("SIGNING_SECRET")):
+        return Response("Forbidden", status_code=403)
 
-    if hasattr(slack_provision, "name"):
-        try:
-            slack_provision.__class__ = globals()[slack_provision.name.replace(" ", "")]
-            slack_provision()
-        except Exception as e:
-            logger.info(f"Error {e}")
-            return Response("", status_code=200)
-    return Response("", status_code=200)
-
+    slack_provision.__class__ = globals()[slack_provision.name.replace(" ", "")]
+    slack_provision()
