a2023e3139ad2eb2e6b3a25095d1bd3d51dcc0eb
Diego Diaz <diegodiazespinoza@gmail.com>
2023-08-08 19:52:18 -0400

    [TEST]

--- slack_approval/slack_provision.py
+++ slack_approval/slack_provision.py
@@ -66,7 +66,7 @@ class SlackProvision:
                 self.is_open_reason_view()
             elif self.action_id == "Not allowed":
                 message = f"Same request/response user {self.user} not allowed. Prevent self approval is on."
-                self.send_message_to_thread(message=message, thread_ts=self.ts, channel=self.approvers_channel)
+                self.send_message_to_thread(message=message, thread_ts=self.ts, channel=self.response_url)
                 self.send_message_to_thread(message=message, thread_ts=self.ts, channel=self.requesters_channel)
             elif self.action_id == "Reject Response":
                 self.rejected()
