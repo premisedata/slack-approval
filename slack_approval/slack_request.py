43c2d3bcad51bd6337a1ce80a89614dee34ca81f
Diego Diaz <diegodiazespinoza@gmail.com>
2023-08-08 19:59:26 -0400

    [TEST]

--- slack_approval/slack_request.py
+++ slack_approval/slack_request.py
@@ -77,6 +77,7 @@ class SlackRequest:
             # Save timestamp and requesters channel to be updated after provision
             self.value["ts"] = response.get("ts")
             self.value["requesters_channel"] = self.requesters_channel
+            self.value["approvers_channel"] = self.approvers_channel
         except errors.SlackApiError as e:
             logger.error(e)
         value = json.dumps(self.value)
