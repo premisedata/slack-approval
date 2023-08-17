91a749a1c4c4b1275dcb6542597c4258078e1839
Diego Diaz <diegodiazespinoza@gmail.com>
2023-08-17 19:42:08 -0400

    [ADD] show modifiables fields

--- slack_approval/slack_provision.py
+++ slack_approval/slack_provision.py
@@ -37,7 +37,6 @@ class SlackProvision:
         elif self.is_callback_view(callback_id="edit_view_modal"):
             self.channel_id = None
             self.reason = None
-            logger.info(self.payload)
             self.get_private_metadata()
             self.action_id = "Modified"
             self.get_modified_fields()
@@ -204,6 +203,7 @@ class SlackProvision:
             logger.error(e)
 
     def open_reject_reason_view(self):
+        logger.info(self.payload)
         private_metadata = {
             "channel_id": self.payload["channel"]["id"],
             "approvers_ts": self.payload["message"]["ts"],
