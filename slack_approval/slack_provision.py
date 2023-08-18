05da24d4fe54369cde9e05e8a9360d3eedea4c8b
Diego Diaz <diegodiazespinoza@gmail.com>
2023-08-18 15:01:59 -0400

    [ADD] show modifiables fields

--- slack_approval/slack_provision.py
+++ slack_approval/slack_provision.py
@@ -320,7 +320,26 @@ class SlackProvision:
             modifiable_field_value,
         ) in self.modifiables_fields.items():
             if isinstance(modifiable_field_value, list):
-                continue
+                blocks.append({"type": "section", "text": {"type": "mrkdwn", "text": "Multi values"}})
+                for value in modifiable_field_value:
+                    field = {
+                        "type": "input",
+                        "block_id": f"block_id_{modifiable_field_name}",
+                        "label": {"type": "plain_text", "text": modifiable_field_name},
+                        "element": {
+                            "type": "plain_text_input",
+                            "action_id": f"action_id_{modifiable_field_name}",
+                            "placeholder": {
+                                "type": "plain_text",
+                                "text": value,
+                            },
+                            "initial_value": value,
+                            "multiline": False,
+                        },
+                        "optional": True,
+                    }
+                    blocks.append(field)
+                blocks.append({"type": "section", "text": {"type": "mrkdwn", "text": "Modifiable fields"}})
             field = {
                 "type": "input",
                 "block_id": f"block_id_{modifiable_field_name}",
