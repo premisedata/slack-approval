2a87c7120fc18be53b9c5a4f49e2ed4042878db5
Diego Diaz <diegodiazespinoza@gmail.com>
2023-08-24 16:10:45 -0400

    [ADD] show modifiables fields

--- slack_approval/slack_request.py
+++ slack_approval/slack_request.py
@@ -39,8 +39,8 @@ class SlackRequest:
                 slack_web_client = WebClient(self.token)
                 user_response = slack_web_client.users_lookupByEmail(email=self.inputs.get("requester"))
                 logger.info(user_response)
-                # if user_response and user_response.status_code == 200:
-                #     self.value["requester_info"] = json.dumps(user_response)
+                if user_response and user_response.status_code == 200:
+                    self.value["requester_info"] = json.dumps(user_response)
             except errors.SlackApiError as e:
                 logger.error(e, stack_info=True, exc_info=True)
 
