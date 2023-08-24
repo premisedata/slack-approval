ba6a98f934d644416af17fd0d01c67cd42bfb642
Diego Diaz <diegodiazespinoza@gmail.com>
2023-08-24 17:03:15 -0400

    [UPDATE] version setup and req

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
-    version="0.1.6",
+    version="0.1.6.1",
     license='MIT',
     classifiers=[
         'License :: OSI Approved :: MIT License',
