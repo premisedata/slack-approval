0b8c7013a9c778856c251855f3510dff4796e0c6
Diego Diaz <diegodiazespinoza@gmail.com>
2023-10-03 09:16:43 -0300

    [ADD] show modifiables fields

--- slack_approval/slack_provision.py
+++ slack_approval/slack_provision.py
@@ -450,7 +450,7 @@ class SlackProvision:
         logger.info(self.inputs)
         old_values = {}
         for block_name, block_values in blocks.items():
-            old_values[re.sub(r"_\d+$", '', block_name)] = self.inputs[re.sub(r"_\d+$", '', block_name)]
+            old_values[re.sub(r"_\d+$", '', block_name)] = self.inputs[re.sub(r"_\d+$", '', block_name)].copy()
             self.inputs[re.sub(r"_\d+$", '', block_name)] = []
         logger.info(old_values)
 
